c136f009ed96791396286add98217ea421599d58
jay grollman <jfg93@cornell.edu>
2017-08-07 17:46:17 -0700

    trying to serve an html file

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var app = express()
+var path = require('path');
 
-app.get('/', function (req, res) {
-	  res.send('Hello Digital Ocean!')
-})
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.listen(3000, function () {
 	  console.log('Magic is happening on port 3000!')
